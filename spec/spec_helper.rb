daf2d82ef7337a26815fd6f6ee217d01c5dea27b
蒼時弦也 <elct9620@frost.tw>
2020-09-04 21:49:00 +0800

    Setup RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'eventmachine'
+require 'simplecov'
+
+SimpleCov.start do
+  load_profile "test_frameworks"
+
+  add_filter %r{^/vendor/}
+  add_filter %r{^/config/}
+  add_filter %r{^/db/}
+
+  add_group "Controllers", "src/controller"
+  add_group "Models", "src/model"
+  add_group "Protocols" do |src|
+    src.filename.include?("src/protocol/") &&
+      !src.filename.include?('command')
+  end
+  add_group "Commands", "src/protocol/command"
+  add_group "Rules", "src/rule"
+  add_group "Libraries", "lib/"
+end
+
+Dir[Bundler.root.join('spec/support/**/*.rb')].sort.each { |support| require support }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
