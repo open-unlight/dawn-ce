163c26ff92a41eef2547925280b232506e236c3e
蒼時弦也 <elct9620@frost.tw>
2020-09-05 01:33:46 +0800

    Setup cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -20,17 +20,3 @@ SimpleCov.start do
   add_group "Rules", "src/rule"
   add_group "Libraries", "lib/"
 end
-
-Dir[Bundler.root.join('spec/support/**/*.rb')].sort.each { |support| require support }
-
-RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-end
