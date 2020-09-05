cb46b94398aebe894dc726c94b19dd5c618c6c15
蒼時弦也 <elct9620@frost.tw>
2020-09-06 01:48:34 +0800

    Use ENV['HOSTNAME'] as SV_IP

--- src/matching.rb
+++ src/matching.rb
@@ -1,7 +1,8 @@
 # Unlight
-# Copyright(c)2019 CPA
-# This software is released under the MIT License.
-# http://opensource.org/licenses/mit-license.php
+# Copyright (c) 2019 CPA
+# Copyright (c) 2019 Open Unlight
+# This software is released under the Apache 2.0 License.
+# https://opensource.org/licenses/Apache2.0
 
 $:.unshift File.expand_path(File.dirname(__FILE__))
 require 'optparse'
@@ -24,13 +25,8 @@ module Unlight
 
   opt.parse! ARGV
 
+  SV_IP = ENV['HOSTNAME']
   $SERVER_NAME = "MATCHING_SV#{SV_PORT}"
-
-  begin
-    SV_IP = "0.0.0.0"
-  rescue => e
-    SERVER_LOG.fatal("GameServer:IP 未設定")
-  end
 end
 
 require 'unlight'
