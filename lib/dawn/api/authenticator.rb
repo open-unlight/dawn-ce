6fd0fe66d33a9eba8b0b2dc21c95a7ff2ac34115
蒼時弦也 <elct9620@frost.tw>
2021-08-20 18:11:13 +0800

    Add get current avatar information api

--- lib/dawn/api/authenticator.rb
+++ lib/dawn/api/authenticator.rb
@@ -31,22 +31,31 @@ module Dawn
         @player = Unlight::Player[@player_id]
       end
 
-      # Verify signature is valid
+      # Calculate Signature
       #
-      # @param signature [String]
-      #
-      # @return [TureClass|FalseClass]
+      # @return [String] signatured value
       #
       # @since 0.1.0
-      def valid?(signature)
-        return false if player&.session_key.nil?
-
+      def signature
         OpenSSL::HMAC
           .hexdigest(
             'SHA256',
             player.session_key,
             "#{@nonce}#{@payload}"
-          ) == signature
+          )
+      end
+
+      # Verify signature is valid
+      #
+      # @param provided_signature [String]
+      #
+      # @return [TureClass|FalseClass]
+      #
+      # @since 0.1.0
+      def valid?(provided_signature)
+        return false if player&.session_key.nil?
+
+         provided_signature == signature
       end
     end
   end
