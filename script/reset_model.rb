c47b7ee1223b3f5053a82f672ae361bc57c8f228
蒼時弦也 <elct9620@frost.tw>
2021-10-28 18:24:54 +0800

    Clear rubocop global vars warning

--- script/reset_model.rb
+++ script/reset_model.rb
@@ -2,26 +2,26 @@
 $LOAD_PATH.unshift(File.join(File.expand_path('.'), 'src'))
 require 'pathname'
 require 'unlight'
-$arg = ARGV.shift
-puts $arg
+arg = ARGV.shift
+
 class String
   def camelize
     split(/[^a-z0-9]/i).map(&:capitalize).join
   end
 end
 
-require "model/#{$arg}"
+require "model/#{arg}"
 
 module Unlight
-  if eval("#{$arg.camelize}.table_exists? # Unlight::CharacterCard.table_exists?", binding, __FILE__, __LINE__)
-    col = eval("#{$arg.camelize}.columns # Unlight::CharacterCard.columns", binding, __FILE__, __LINE__)
-    puts "既存のモデルデータ#{$arg}のテーブルをリセットしますか(#{col})(y/n)"
+  if eval("#{arg.camelize}.table_exists? # Unlight::CharacterCard.table_exists?", binding, __FILE__, __LINE__)
+    col = eval("#{arg.camelize}.columns # Unlight::CharacterCard.columns", binding, __FILE__, __LINE__)
+    puts "既存のモデルデータ#{arg}のテーブルをリセットしますか(#{col})(y/n)"
     answer = gets.chomp
     if answer == 'y'
-      eval("#{$arg.camelize}.create_table! # Unlight::CharacterCard.create_table!", binding, __FILE__, __LINE__)
+      eval("#{arg.camelize}.create_table! # Unlight::CharacterCard.create_table!", binding, __FILE__, __LINE__)
     end
   else
     puts 'モデルデータが存在しないのでテーブルを作ります'
-    eval("#{$arg.camelize}.create_table # Unlight::CharacterCard.create_table", binding, __FILE__, __LINE__)
+    eval("#{arg.camelize}.create_table # Unlight::CharacterCard.create_table", binding, __FILE__, __LINE__)
   end
 end
