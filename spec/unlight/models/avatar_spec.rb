a47ff6e762a12950288edacbc42331a82f97f2f9
蒼時弦也 <elct9620@frost.tw>
2021-11-09 13:38:18 +0800

    Add tests for Avatar#achievement_check

--- spec/unlight/models/avatar_spec.rb
+++ spec/unlight/models/avatar_spec.rb
@@ -19,16 +19,110 @@ RSpec.describe Unlight::Avatar do
       expect(event).to have_received(:update_achievement_info_event).with('', '', '', '', '')
     end
 
-    context 'with started achievement inventory' do
+    context 'when achievement inventory is started' do
       let(:achievement) { create(:achievement) }
       let!(:inventory) { create(:achievement_inventory, avatar_id: avatar.id, achievement_id: achievement.id, state: Unlight::ACHIEVEMENT_STATE_START) }
 
       it do
         check
         args = [
-          inventory.achievement_id.to_s, inventory.state.to_s, inventory.progress.to_s, '', inventory.code.to_s
+          inventory.achievement_id.to_s, Unlight::ACHIEVEMENT_STATE_START.to_s, inventory.progress.to_s, '', inventory.code.to_s
         ]
         expect(event).to have_received(:update_achievement_info_event).with(*args)
+      end
+    end
+
+    context 'when achievement inventory condition is passed' do
+      let(:achievement) { create(:achievement) }
+      let!(:inventory) { create(:achievement_inventory, avatar_id: avatar.id, achievement_id: achievement.id, state: Unlight::ACHIEVEMENT_STATE_START) }
+
+      before do
+        allow_any_instance_of(Unlight::Achievement).to receive(:cond_check).and_return(true) # rubocop:disable RSpec/AnyInstance
+      end
+
+      it do
+        check
+        args = [
+          inventory.achievement_id.to_s, Unlight::ACHIEVEMENT_STATE_FINISH.to_s, inventory.progress.to_s, '', inventory.code.to_s
+        ]
+        expect(event).to have_received(:update_achievement_info_event).with(*args)
+      end
+    end
+
+    context 'when achievement have items' do
+      let(:achievement) { create(:achievement, items: '0/0/0/0') }
+
+      before do
+        create(:achievement_inventory, avatar_id: avatar.id, achievement_id: achievement.id, state: Unlight::ACHIEVEMENT_STATE_START)
+        allow_any_instance_of(Unlight::Achievement).to receive(:cond_check).and_return(true) # rubocop:disable RSpec/AnyInstance
+      end
+
+      it do
+        check
+        expect(event).to have_received(:achievement_clear_event).with(achievement.id, 0, 0, 0, 0)
+      end
+    end
+
+    context 'when achievement have items but no event' do
+      let(:achievement) { create(:achievement, items: '0/0/0/0') }
+
+      before do
+        create(:achievement_inventory, avatar_id: avatar.id, achievement_id: achievement.id, state: Unlight::ACHIEVEMENT_STATE_START)
+        allow_any_instance_of(Unlight::Achievement).to receive(:cond_check).and_return(true) # rubocop:disable RSpec/AnyInstance
+        avatar.instance_variable_set(:@event, nil)
+        allow(avatar).to receive(:write_notice)
+      end
+
+      it do
+        check
+        expect(avatar).to have_received(:write_notice).with(Unlight::NOTICE_TYPE_ACHI_SUCC, "#{achievement.id},0_0_0_0")
+      end
+    end
+
+    context 'when achievement have selectable items' do
+      let(:achievement) { create(:achievement, items: 'S0/0/0/0') }
+
+      before do
+        create(:achievement_inventory, avatar_id: avatar.id, achievement_id: achievement.id, state: Unlight::ACHIEVEMENT_STATE_START)
+        allow_any_instance_of(Unlight::Achievement).to receive(:cond_check).and_return(true) # rubocop:disable RSpec/AnyInstance
+        allow(avatar).to receive(:write_notice)
+      end
+
+      it do
+        check
+        expect(avatar).to have_received(:write_notice).with(Unlight::NOTICE_TYPE_GET_SELECTABLE_ITEM, achievement.id.to_s)
+      end
+    end
+
+    context 'when achievement can loop' do
+      let(:achievement) { create(:achievement, loop: 5) }
+
+      before do
+        create(:achievement_inventory, avatar_id: avatar.id, achievement_id: achievement.id, state: Unlight::ACHIEVEMENT_STATE_START)
+        allow_any_instance_of(Unlight::Achievement).to receive(:cond_check).and_return(true) # rubocop:disable RSpec/AnyInstance
+        allow(avatar).to receive(:add_loop_achievement)
+      end
+
+      it do
+        check
+        expect(avatar).to have_received(:add_loop_achievement)
+      end
+    end
+
+    context 'when achievement loop stop' do
+      subject(:check) { avatar.achievement_check(false, nil, 0, true, true) }
+
+      let(:achievement) { create(:achievement, loop: 5) }
+
+      before do
+        create(:achievement_inventory, avatar_id: avatar.id, achievement_id: achievement.id, state: Unlight::ACHIEVEMENT_STATE_START)
+        allow_any_instance_of(Unlight::Achievement).to receive(:cond_check).and_return(true) # rubocop:disable RSpec/AnyInstance
+        allow(avatar).to receive(:stop_loop_achievement)
+      end
+
+      it do
+        check
+        expect(avatar).to have_received(:stop_loop_achievement)
       end
     end
   end
